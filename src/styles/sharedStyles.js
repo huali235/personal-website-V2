7aa70bded51985dfeabf67cc5850e234410bf2ba
Hussain Ali <huali235@gmail.com>
2024-09-03 21:58:03 -0400

    shared styles component

---
+++ src/styles/sharedStyles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Heading = styled.h1`
+font-family: "Montserrat", sans-serif;
+  font-weight: 700;
+  display: flex;
+  font-size: 1.5rem;
+`;
+
+export const Container = styled.div`
+font-family: "Montserrat", sans-serif;
+  padding-right: 2.5rem;
+  padding-left: 2.5rem;
+  padding-top: 8rem;
+  padding-bottom: 12rem;
+`;
