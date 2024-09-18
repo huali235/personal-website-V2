29e7b2bf7f08acfd10214ddd8dfda61145eda8dc
Hussain Ali <huali235@gmail.com>
2024-09-18 14:26:46 -0400

    added icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import styled from "styled-components";
+import { IconLink, IconLinks } from "../styles/sharedStyles";
+
+const FooterContainer = styled.footer`
+  margin-top: 2rem;
+  padding-right: 2.5rem;
+  padding-left: 2.5rem;
+  padding-bottom: 1.5rem;
+`;
+const IconContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+`;
+
 function Footer() {
   return (
-    <footer>
-      <p>© 2021</p>
-    </footer>
+    <FooterContainer>
+      <IconContainer>
+        <IconLinks>
+          <IconLink
+            href="https://github.com/huali235"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </IconLink>
+          <IconLink
+            href="https://www.linkedin.com/in/hussain-ali-6ab559298/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </IconLink>
+        </IconLinks>
+      </IconContainer>
+    </FooterContainer>
   );
 }
 
